58ebae851da4deb8be2b642250e59ae24b52a4ca
Sahar Ahsan <163599337+Saharahsan22@users.noreply.github.com>
2024-04-02 21:54:39 +0500

    Add files via upload
    hello world with name

---
+++ main.js
@@ -0,0 +1,4 @@
+//1st assignment of Hello World with name
+var personName = 'Sahar Ahsan';
+var message = 'Hello World with ' + personName;
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//1st assignment of Hello World with name
+
+let personName = 'Sahar Ahsan';
+let message = 'Hello World with ' + personName
+
+console.log(message);
